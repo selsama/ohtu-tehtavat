1d5d4ffe4e8e4b5b820e4723a181e219edd719e9
Sonja Salmi <salmi.sonjakaroliina@gmail.com>
2020-12-07 22:58:35 +0200

    viikko6 teht4

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+/**
+ *
+ * @author salmison
+ */
+public class QueryBuilder {
+    
+    private Matcher matcher;
+    
+    public QueryBuilder() {
+        this.matcher = new All();
+    }
+    
+    public Matcher build() {
+        return matcher;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category) {
+        matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category) {
+        matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
+    }
+    
+    
+    
+}
